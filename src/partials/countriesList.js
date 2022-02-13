2d9eb8eecd33508a5048a5f376973051037a65d1
Olia Rosiak <olia.rosiak@gmail.com>
2022-02-13 14:07:02 +0200

    countriesList / countryCard add destr

--- src/partials/countriesList.js
+++ src/partials/countriesList.js
@@ -1,7 +1,7 @@
 export function markupForCountriesList(countries) {
   
-    const countryListLayout = countries.map(({ name, flags }) => {
-        return `<li class="list-item"><img class="flag-pic" src="${flags.svg}" alt="${name.official}" /><span class="name">${name.official}</span></li>`
+    const countryListLayout = countries.map(({ name: {official}, flags: {svg} }) => {
+        return `<li class="list-item"><img class="flag-pic" src="${svg}" alt="${official}" /><span class="name">${official}</span></li>`
     }).join('');
 
     return countryListLayout;  
--- src/partials/countryCard.js
+++ src/partials/countryCard.js
@@ -1,8 +1,8 @@
 export function markupForCounty(country) {
     
-    const countryLayout = country.map(({ name, capital, population, flags, languages }) => {
+    const countryLayout = country.map(({ name: {official}, capital, population, flags: {svg}, languages }) => {
         return `
-        <p class="wrap"><img class="flag-pic" src="${flags.svg}" alt="${name.official}" /><span class="name">${name.official}</span></p>
+        <p class="wrap"><img class="flag-pic" src="${svg}" alt="${official}" /><span class="name">${official}</span></p>
         <p class="wrap">Capital: <span class="wrap-text">${capital}</span></p>
         <p class="wrap">Population: <span class="wrap-text">${population}</span></p>
         <p class="wrap">Languages: <span class="wrap-text">${Object.values(languages).join(', ')}</span></p>
